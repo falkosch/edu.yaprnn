ff27d1a6462cc5d1898e6f1c0bf8b74f2d619038
Falko Schwabe <falko.schwabe@job-machine.de>
2024-12-21 03:57:12 +0100

    Heatmap-like weights visualization with dynamic color interpolation
    Replaces static intensity calculation with dynamic heat color interpolation using a lookup table. Improves understanding the weights map representation. Adjusted gamma handling to streamline calculations.

--- src/main/java/edu/yaprnn/gui/services/VisualizationService.java
+++ src/main/java/edu/yaprnn/gui/services/VisualizationService.java
@@ -15,7 +15,23 @@ import javax.swing.table.DefaultTableModel;
 @Singleton
 public class VisualizationService {
 
-  private static final DecimalFormat TABLE_VALUE_FORMAT = new DecimalFormat("0.###E0");
+  private static final DecimalFormat TABLE_VALUE_FORMAT = new DecimalFormat(" 0.0###;-0.0###");
+  private static final float[][] SCIENTIFIC_COLORS_BERLIN_LOOKUP = {
+      // big negative
+      {-1.25f, 158f, 176f, 255f},
+      // small negative
+      {-0.5f, 40f, 104f, 134f},
+      // near zero
+      {+0f, 28f, 11f, 6f},
+      // small positive
+      {+0.5f, 128f, 54f, 32f},
+      // big positive
+      {+1.25f, 255f, 173f, 173f}};
+  private static final float[][] HEAT_LOOKUP = SCIENTIFIC_COLORS_BERLIN_LOOKUP;
+  private static final int HEAT_LOOKUP_INTENSITY = 0;
+  private static final int HEAT_LOOKUP_RED = 1;
+  private static final int HEAT_LOOKUP_GREEN = 2;
+  private static final int HEAT_LOOKUP_BLUE = 3;
 
   public Image fromOutput(float[] output, int width, float zoom, float gamma) {
     var height = Math.max(1, inputSizeWithBias(output, width));
@@ -49,14 +65,15 @@ public class VisualizationService {
     var image = new BufferedImage(outputSize, height, BufferedImage.TYPE_INT_RGB);
 
     try {
-      var gammaAsDouble = (double) gamma;
+      var gammaAsDouble = (double) -gamma;
       for (int y = 0, w = 0; y < height; y++) {
         for (var x = 0; x < outputSize; x++, w++) {
           var weight = weights[w];
           var sign = Math.signum(weight);
-          var positiveGammaAdjusted = Math.pow(sign * weight, gammaAsDouble);
-          var intensity = Math.clamp(Math.round(255d - positiveGammaAdjusted), 0, 255);
-          image.setRGB(x, y, intensity << (sign < 0 ? 0 : 8) | intensity << (sign > 0 ? 16 : 8));
+          var absoluteWeight = sign * weight;
+          var gammaAdjusted =
+              absoluteWeight > 0f ? (float) Math.pow(sign * weight, gammaAsDouble) : 0f;
+          image.setRGB(x, y, toHeatColor(sign * gammaAdjusted));
         }
       }
 
@@ -66,6 +83,39 @@ public class VisualizationService {
     } finally {
       image.flush();
     }
+  }
+
+  private static int toHeatColor(float intensity) {
+    var startHeat = HEAT_LOOKUP[intensity < 0f ? 0 : HEAT_LOOKUP.length - 1];
+    var redFloat = startHeat[HEAT_LOOKUP_RED];
+    var greenFloat = startHeat[HEAT_LOOKUP_GREEN];
+    var blueFloat = startHeat[HEAT_LOOKUP_BLUE];
+
+    for (int minIndex = 0, maxIndex = 1; maxIndex < HEAT_LOOKUP.length; minIndex++, maxIndex++) {
+      var minHeat = HEAT_LOOKUP[minIndex];
+      var minHeatIntensity = minHeat[HEAT_LOOKUP_INTENSITY];
+      var maxHeat = HEAT_LOOKUP[maxIndex];
+
+      var maxHeatIntensity = maxHeat[HEAT_LOOKUP_INTENSITY];
+      if (intensity < minHeatIntensity || intensity >= maxHeatIntensity) {
+        continue;
+      }
+
+      var minHeatRed = minHeat[HEAT_LOOKUP_RED];
+      var minHeatGreen = minHeat[HEAT_LOOKUP_GREEN];
+      var minHeatBlue = minHeat[HEAT_LOOKUP_BLUE];
+
+      var factor = (intensity - minHeatIntensity) / (maxHeatIntensity - minHeatIntensity);
+      redFloat = minHeatRed + factor * (maxHeat[HEAT_LOOKUP_RED] - minHeatRed);
+      greenFloat = minHeatGreen + factor * (maxHeat[HEAT_LOOKUP_GREEN] - minHeatGreen);
+      blueFloat = minHeatBlue + factor * (maxHeat[HEAT_LOOKUP_BLUE] - minHeatBlue);
+      break;
+    }
+
+    var red = Math.clamp(Math.round(redFloat), 0, 255);
+    var green = Math.clamp(Math.round(greenFloat), 0, 255);
+    var blue = Math.clamp(Math.round(blueFloat), 0, 255);
+    return (red << 16) | (green << 8) | blue;
   }
 
   public DefaultTableModel classificationTableModel(String[] labels, Layer[] layers,
