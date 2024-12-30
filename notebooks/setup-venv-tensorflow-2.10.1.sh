aeaa2c8345737dd6b5e8936c2e70c279d99be1bb
Falko Schwabe <falko.schwabe@job-machine.de>
2024-12-30 02:56:24 +0100

    Align network examples with the ones from the notebooks

--- notebooks/setup-venv-tensorflow-2.10.1.sh
+++ notebooks/setup-venv-tensorflow-2.10.1.sh
@@ -5,4 +5,4 @@ set -x
 [ -d .venv-tf ] && rm -rf .venv-tf
 python -m venv --copies --clear --upgrade-deps .venv-tf || exit 1
 . .venv-tf/Scripts/activate || exit 1
-pip install -r requirements-tf.txt || exit 1
+pip install -r requirements-tensorflow-2.10.1.txt || exit 1
